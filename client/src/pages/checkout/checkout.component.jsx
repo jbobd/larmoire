d5440193cafca77bf1ba203f282cb3b1e07049cb
juan bobadilla <yarboclos@MacBook-Pro-de-juan.local>
2020-03-25 22:12:00 -0300

    added clear cart item in check out page

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectCartItems, selectCartTotal } from "../../redux/cart/cart.selectors";
+import {
+  selectCartItems,
+  selectCartTotal
+} from "../../redux/cart/cart.selectors";
+
+import { clearCart } from "../../redux/cart/cart.actions";
 
 import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
-import  CheckoutItem  from "../../components/checkout-item/checkout-item.component";
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import CustomButton from "../../components/custom-button/custom-button.component";
+
 import "./checkout.styles.scss";
 
-const CheckoutPage = ({cartItems,total, clearCart}) => (
+const CheckoutPage = ({ cartItems, total, clearCart }) => (
   <div className="checkout-page">
     <div className="checkout-header">
       <div className="header-block">
@@ -26,26 +33,30 @@ const CheckoutPage = ({cartItems,total, clearCart}) => (
         <span>Remove</span>
       </div>
     </div>
-    {
-        cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)
-    }
-    
+    {cartItems.map(cartItem => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
 
-     <div className="total">TOTAL: ${total}</div>
-     <div className="test-warning">
-       *Please use the following credit card for payments: 
-       <br/>
-       4242 4242 4242 4242
-       exp. date: 01/01 - CVV: 123
-     </div>
-     <StripeCheckoutButton price={total}/>
+    <div className="total">TOTAL: ${total}</div>
+    <div className="test-warning">
+      *Please use the following credit card for payments:
+      <br />
+      4242 4242 4242 4242 exp. date: 01/01 - CVV: 123
+    </div>
+    <div className="payclear-buttons">
+      <CustomButton className="clear-button" onClick={() => clearCart()}>Clear cart</CustomButton>
+      <StripeCheckoutButton className="pay-button" price={total} />
+    </div>
   </div>
 );
 
-const mapStateToProps = createStructuredSelector ({
-    cartItems: selectCartItems,
-    total: selectCartTotal
-})
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal
+});
 
+const mapDispatchToProps = dispatch => ({
+  clearCart: () => dispatch(clearCart())
+});
 
-export default connect(mapStateToProps)(CheckoutPage);
+export default connect(mapStateToProps, mapDispatchToProps)(CheckoutPage);
